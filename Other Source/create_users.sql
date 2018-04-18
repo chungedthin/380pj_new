793574af70b3448c6959b074a6a08b22e8c9287d
chung <chung@DESKTOP-VQ02SEG>
2018-04-18 14:28:00 +0800

    CREATE db

--- Other Source/create_users.sql
+++ Other Source/create_users.sql
@@ -1,3 +1,23 @@
+CREATE TABLE users (
+username VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
+PRIMARY KEY (username)
+);
+CREATE TABLE user_roles (
+user_role_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+username VARCHAR(50) NOT NULL,
+role VARCHAR(50) NOT NULL,
+PRIMARY KEY (user_role_id),
+FOREIGN KEY (username) REFERENCES users(username)
+);
+INSERT INTO users VALUES ('admin', 'adminpw');
+INSERT INTO user_roles(username, role) VALUES ('admin', 'ROLE_ADMIN');
+
+INSERT INTO users VALUES ('user1', 'user1pw');
+INSERT INTO user_roles(username, role) VALUES ('user1', 'ROLE_USER');
+
+
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
